7678d1b9940aa5fe259fc8ed47e1ee087d0cfeb2
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-10-04 04:30:02 +0530

    initial setup - 2 : with bootstrap installation

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './app/Layout/Header';
+import FooterMarginTop from './app/Layout/FooterMaginTop';
+import Footer from './app/Layout/Footer';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <FooterMarginTop />
+    <Footer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
